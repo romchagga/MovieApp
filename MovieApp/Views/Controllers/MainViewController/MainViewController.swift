763631bc6ac62996c96844f989fb818a7c035eec
Ramis Askerov <askerov.ramis1@gmail.com>
2023-01-10 18:03:45 +0300

    prepare for working without storyboards

---
+++ MovieApp/Views/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  MovieApp
+//
+//  Created by macbook on 10.01.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        self.title = "Main"
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
